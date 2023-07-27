f036fdcf357472a7e90dc27d79c7fdad89f77991
Abdulrahman Omar <96279885+egyptianego17@users.noreply.github.com>
2023-07-27 16:51:29 +0300

    RCC Peripheral

---
+++ 2- MCAL/1-RCC/RCC_Config.h
@@ -0,0 +1,34 @@
+/*****************************************************/
+/*                 SWC: RCC Driver                   */
+/*            Author: Abdulrahman Omar               */
+/*                 Version: v 1.0                    */
+/*              Date: 21 July - 2023                 */
+/*****************************************************/
+
+/* File Gaurd */
+#ifndef RCC_CONFIG_H
+#define RCC_CONFIG_H
+
+/* Options for Clk Source (HSI - HSE - PLL)                */
+#define RCC_CLK_SRC PLL
+
+/* Options for PLL Source (HSI - HSE)                      */
+#define RCC_PLL_SRC HSI
+
+/* PLLM: REG_Range 2 ≤ PLLM ≤ 63
+   0.5≤Fsrc(Mhz)/PLLM≤1 
+   Recommended to be 0.5 × Fsrc(Mhz)                       */
+#define RCC_PLLM 8
+
+/* PLLN: REG_Range 192 ≤ PLLN ≤ 432 
+   192≤(PLLN×Fsrc(Mhz)/PLLM)≤432, Max: PLLM × Fsrc(Mhz)    */
+#define RCC_PLLN 210
+
+/* PLLP Options: 0: PLLP = 2
+                 1: PLLP = 4
+                 2: PLLP = 6
+                 3: PLLP = 8 
+   PLLN×Fsrc(Mhz)/(PLLM*PLLP) Must not exceed 84 MHz       */
+#define RCC_PLLP 2
+
+#endif
