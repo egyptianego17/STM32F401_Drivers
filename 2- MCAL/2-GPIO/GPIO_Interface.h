55144c93ea15581fba6ee1f76f4d7361021e522a
Abdulrahman Omar <96279885+egyptianego17@users.noreply.github.com>
2023-07-31 22:09:02 +0300

    Update GPIO_Interface.h

--- 2- MCAL/2-GPIO/GPIO_Interface.h
+++ 2- MCAL/2-GPIO/GPIO_Interface.h
@@ -12,44 +12,52 @@
 #ifndef GPIO_INTERFACE_H
 #define GPIO_INTERFACE_H
 
+/* Function Prototypes */
 void GPIO_voidDefaultInit();
 void GPIO_voidSetPinMode(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinMode);
 void GPIO_voidSetPinType(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinType);
 void GPIO_voidSetPinSpeed(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinSpeed);
-void GPIO_voidSetPinPUPD(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinPUPDOption );
+void GPIO_voidSetPinPUPD(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinPUPDOption);
 void GPIO_voidSetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinValue);
 void GPIO_voidSetPinValueDirectAccess(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8PinValue);
 void GPIO_voidSetPortValue(u8 Copy_u8PortId, u16 Copy_u8PortValue);
 u8   GPIO_u8GetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId);
 
-
+/* GPIO Port IDs */
 #define IOA        0
 #define IOB        1
 #define IOC        2
- 
+
+/* GPIO Pin Modes */
 #define INPUT      0
 #define OUTPUT     1
 #define AF         2
 #define ANALOG     3
 
+/* GPIO Pin Output Types */
 #define OUTPUT_PP  0
 #define OUTPUT_OD  1
 
+/* GPIO Pin Output Speeds */
 #define OUTPUT_LS  0
 #define OUTPUT_MS  1
 #define OUTPUT_HS  2
 #define OUTPUT_VHS 3
 
+/* GPIO Pin Logic Levels */
 #define HIGH       1
 #define LOW        0
 
+/* GPIO Pin Value Set Options */
 #define SET        0
 #define RST        1
 
+/* GPIO Pin Pull-Up/Pull-Down Configurations */
 #define FLOATING   0
 #define INPUT_PU   1
 #define INPUT_PD   2
 
+/* GPIO Pin Numbers */
 #define PIN0       0
 #define PIN1       1
 #define PIN2       2
