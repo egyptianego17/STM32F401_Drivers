0ef8019d5141131760851c47f988237c1857fd80
Abdulrahman Omar <96279885+egyptianego17@users.noreply.github.com>
2023-08-02 00:51:28 +0300

    Update BIT_MATH.h

--- 1- LIB/BIT_MATH.h
+++ 1- LIB/BIT_MATH.h
@@ -7,7 +7,9 @@
 #define GET_BIT(VAR,BITNO) (((VAR) >> (BITNO)) & 0x01)
 
 #define MODIFY_REGISTER_BITS(REG, START_BIT, END_BIT, VALUE) \
-    u32 BITMASK = ((1U << ((END_BIT) - (START_BIT) + 1)) - 1) << (START_BIT); \
-    (REG) = ((REG) & ~BITMASK) | (((VALUE) << (START_BIT)) & BITMASK); \
+    do { \
+        u32 bitmask = ((1U << ((END_BIT) - (START_BIT) + 1)) - 1) << (START_BIT); \
+        (REG) = ((REG) & ~bitmask) | (((VALUE) << (START_BIT)) & bitmask); \
+    } while (0)
 
 #endif
